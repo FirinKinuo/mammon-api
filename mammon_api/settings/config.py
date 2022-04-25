c1eb636cf9e0710b113daed8573d48483ca68f33
FirinKinuo <deals@fkinuo.ru>
2022-04-25 14:45:03 +0300

    Update: Optimize RAM usage

--- mammon_api/settings/config.py
+++ mammon_api/settings/config.py
@@ -1,19 +1,12 @@
 """Module configuration"""
-import sys
 import logging
 
 from os import environ
-from pathlib import Path
 
-IS_TEST = any(map(lambda path: 'tests' in path, sys.path))  # If test paths are found, then go to test mode
+DEBUG = bool(int(environ.get('DEBUG') or False))
+LOG_LEVEL = logging.getLevelName((environ.get('LOG_LEVEL') or 'info' if not DEBUG else 'debug').upper())
 
-DEBUG = bool(int(environ.get('DEBUG'))) or False
-LOG_LEVEL = logging.getLevelName((environ.get('LOG_LEVEL') if not DEBUG else 'debug').upper())
-
-API_HOST = environ.get('API_HOST')
-API_PORT = int(environ.get('API_PORT'))
-
-SQLITE_ENGINE = f"sqlite:///{Path('/data', 'mammon.sqlite3')}"
+SQLITE_ENGINE = f"sqlite:////data/mammon.sqlite3"
 
 logging.basicConfig(level=LOG_LEVEL,
                     format="[%(asctime)s] %(levelname)s [%(funcName)s] %(message)s",
